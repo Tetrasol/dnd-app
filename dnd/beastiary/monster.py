132d854d0daa5d74e1c109daa86372f16e1a32a0
Omar Rivera <gomarivera@gmail.com>
2015-08-15 16:24:19 -0500

    basic beastiary additions

--- dnd/beastiary/monster.py
+++ dnd/beastiary/monster.py
@@ -1,7 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/pthon
+from monster_list import MonsterList
+MONSTER_LIST = MonsterList()
+
 class Monster(object):
-  
-  def __init__(self, name, max_HP, current_HP):
-    self.name = name
-    self.max_HP = max_HP
-    self.current_HP = current_HP
+
+    def __init__(self, name, max_HP):
+        self.name = name
+        self.monster_ID = None
+        self.max_HP = max_HP
+        self.current_HP = max_HP
+
+    def addMonsterIDToIDList(self):
+        self.monster_ID = self.name.upper()
+
+        if len(MONSTER_LIST.IDs) == 0: # if the list is empty
+            # add monster id to list
+        elif len(MONSTER_LIST.IDs) > 0: # list not empty
+            # make sure the id is unique
+            for ID in MONSTER_LIST.IDs:
+        else:
+            pass
--- dnd/campaignmanager/encounter.py
+++ dnd/campaignmanager/encounter.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
-from event_check import EventCheck
+#from event_check import EventCheck
 
 ## An Encounter is described as a collections of tasks needed to be completed
 ## by the players. The Encounters only exist within the context of a Quest.
 class Encounter(object):
 
-  # textual encounter plot, setting, and objective
-  description = None
+    # textual encounter plot, setting, and objective
+    __description = None
 
-  monster_fight = False
-  monster = None # monster list object
+    __monster = None # monster list object
 
-  # event checks are a variety of triggers
-  # they depend on various conditions defined by the dungeon master
-  event_check = None # need Event Object
-  success_condition = None
-  failure_condition = None
+    # event checks are a variety of triggers
+    # they depend on various conditions defined by the dungeon master
+    __event_check = None # need Event Object
+    __success_condition = None
+    __failure_condition = None
 
-  # inventory object describing rewards after encounter completion
-  loot_drop = None # items, xp, money
+    # inventory object describing rewards after encounter completion
+    __loot_drop = None # items, xp, money
 
-  def __init__(self, title):
-    self.title = title
+    def __init__(self, title, monster_fight):
+        self.title = title
+        self.__monster_fight = False
 
-  def addMonsterToEncounter(self):
+    def addMonsterToEncounter(self, monster_id):
+        if monster_fight == True:
+            # add monster
+        else:
+            # handle no monster error
