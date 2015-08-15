613e075647e162a13a23e99a80519648b0c93a1e
gariveradlt <garivera89@gmail.com>
2015-08-15 16:27:24 -0500

    added route to character creation
    Added Character creation to the routes post.
    made dnd a packet

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 #!/usr/bin/env python
 #
-from google.appengine.api import users
-import campCampaignManager
-import webapp2
+#from google.appengine.api import users
+import webapp2, cgi, json, Except
+
+from dnd.character.character import Character
+
+
+################### Character ########################
+
+class CharacterCreaterHandler(webapp2.RequestHandler):
+    def post(self):
+        try:
+            character_data = json.loads(self.request.body)
+            new_adventurer = Character(
+                character_data['player_name'],
+                character_data['character_name'],
+                character_data['race'],
+                character_data['character_class']
+            )
+            print new_adventurer.player_name
+        except Exception:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('potato')
+
+class CharacterAttributeHandler(webapp2.RequestHandler):
+    def get(self):
+        pass #some data
+    def post(self):
+        pass # tomato 
+
+class CharacterStatsHandler(webapp2.RequestHandler):
+    def get(self):
+        pass #potatos
+    def post(self):
+        pass  #poTato
+
+######################################################
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('potato')
+        #user = users.get_current_user()
 
-        if user:
-            self.response.write('Hello ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        #if user:
+            #self.response.write('Hello ' + user.nickname())
+        #else:
+            #self.redirect(users.create_login_url(self.request.uri))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-    ('/campaign', CampaignManager)
+    ('/', MainHandler),
+    ('/api/createcharacter', CharacterCreaterHandler),
+    #('/campaign', CampaignManager)
 ], debug=True)
